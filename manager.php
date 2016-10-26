7b20d1fb2cb3f223b49c14690d15dd9f129d4083
ichbinryan <rread@nmsu.edu>
2016-10-26 12:57:10 -0600

    Initial commit

---
+++ manager.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Barnes & Read Leasing</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+    <h1>Manager</h1>
+    <p>Lets look at some maintenance requests</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <h3>Tenant</h3>
+            <p>You can access your account here</p>
+            <a class="w3-btn" href="https://google.com">Tenant</a>
+        </div>
+        <div class="col-sm-4">
+            <h3>Manager</h3>
+            <p>Manage properties, handle accounts, and more</p>
+            <a class="w3-btn" href="manager.php">Manager</a>
+        </div>
+        <div class="col-sm-4">
+            <h3>Employee</h3>
+            <p>Access necesary information to get stuff done...</p>
+            <a class="w3-btn" href="https://google.com">Employee</a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
