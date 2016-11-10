482ca0c8a95fce942121f28e98d3e3e0f73ec0b1
ichbinryan <rread@nmsu.edu>
2016-11-10 15:17:09 -0700

    Payment and payment info finished. -Ryan

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rread
+ * Date: 11/10/16
+ * Time: 3:07 PM
+ */
+echo 'Logged out';
+
+unset($_SESSION['userid']);
+session_destroy();
+
+
+echo '<script type="text/javascript">';
+echo 'alert("Logged out.");';
+echo 'document.location.href="index.php";';
+echo '</script>';
+
+
+?>
--- tenant/viewAccount.php
+++ tenant/viewAccount.php
@@ -7,12 +7,14 @@ include('../Constants.php');
 ?>
 <head>
     <title>Tenant Account</title>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/main.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <a href='../logout.php'>Click here to log out</a>
 </head>
 <body>
 
